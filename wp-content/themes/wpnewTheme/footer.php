02792bbf48a4ecb76e32e0438c5dc7e6848c55af
Thant Zaw <thantzawzinlin@gmail.com>
2020-02-12 01:00:39 +0630

    displaying post and making header.php and footer.php to sperate the header and footer respectively

---
+++ wp-content/themes/wpnewTheme/footer.php
@@ -0,0 +1,8 @@
+ <footer>
+            <div class="container">
+                <p>Copy Right&copy; <?php the_time("Y");?></p>   
+            </div>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
--- wp-content/themes/wpnewTheme/index.php
+++ wp-content/themes/wpnewTheme/index.php
@@ -1,12 +1,22 @@
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset');?>">
-   
-    <title><?php bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-    <?php wp_head(); ?>
-</head>
-<body>
-    <h1></h1>
-</body>
-</html>
+<?php get_header(); ?>
+    <div class="main">
+        <div class="container">
+            <?php if(have_posts()):?>
+                <?php while(have_posts()):the_post(); ?>
+                    <h3><?php the_title(); ?></h3>
+                    <div class="meta">
+                    Created By <?php the_author(); ?>
+                    On <?php the_date("F j, Y, g:i a"); ?>
+                    </div>
+                    <?php the_content(); ?>
+                <?php endwhile; ?>
+            <?php else : ?>
+
+            <?php echo wpautop('Sorry,NO posts were found');?>
+
+
+
+            <?php endif;?>
+        </div>
+    </div>
+   <?php get_footer(); ?>
